52ed60483ab6c5c24a5070715efb0d7b2f0e0594
George Gillams <george.gillams@skyscanner.net>
2020-06-15 22:59:23 +0100

    [NO-JIRA] Fix errors

--- helpers/regexConstants.js
+++ helpers/regexConstants.js
@@ -8,6 +8,7 @@ const REDIRECT_REGEX = /^[A-Za-z0-9\\.\\ \-\\_#\\/]*$/gi;
 const NAME_REGEX = /^[A-Za-z\\ ]*$/gi;
 const MONZOME_LINK_REGEX = /^(https?:\/\/)?monzo\.me\/[A-Za-z_-]+(\/.*)?$/gi;
 const NON_EMOJI_REGEX = /[A-Za-zä\\ 0-9,]*/gi;
+// eslint-disable-next-line max-len
 const EMAIL_REGEX = /^([a-zA-Z0-9_\-\\.\\+]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/gi;
 const PASSWORD_REGEX = /.{6,}/gi;
 const MD_COMPLETE_REGEX = /.*/gi; // TODO UPDATE
