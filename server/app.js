d4fadd18d447b935fb2a58aa23feb6cfe90285c4
George Gillams <george.gillams@skyscanner.net>
2020-06-15 21:34:57 +0100

    [NO-JIRA] Add SEO server files

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-const { setConfig } = require('next/config');
-setConfig(require('../next.config'));
+import { setConfig } from 'next/config';
+import nextConfig from '../next.config';
+import seo from './seo';
 
 const express = require('express');
 const next = require('next');
 const { Signale } = require('signale');
+
+setConfig(nextConfig);
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -21,6 +24,7 @@ const signale = new Signale(options);
   await app.prepare();
   const server = express();
 
+  server.use(seo);
   server.use('/static', express.static('public/static'));
 
   server.get('*', (req, res) => handle(req, res));
