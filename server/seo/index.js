efe94bf3c0a5263409ef5a6250627e7e2a4477f6
George Gillams <george.gillams@skyscanner.net>
2020-06-16 11:26:06 +0100

    [NO-JIRA] Move seo server files

--- server/seo/index.js
+++ server/seo/index.js
@@ -5,13 +5,13 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/robots.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, './server_content', 'robots.txt'), {
+  res.sendFile(path.join(__dirname, 'robots.txt'), {
     headers: { 'Content-Type': 'text/plain' },
   });
 });
 
 router.get('/sitemap.xml', (req, res) => {
-  res.sendFile(path.join(__dirname, './server_content', 'sitemap.xml'), {
+  res.sendFile(path.join(__dirname, 'sitemap.xml'), {
     headers: { 'Content-Type': 'text/xml' },
   });
 });
