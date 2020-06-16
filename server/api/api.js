01544a8e8fb335fcdbabef956948ccfbdf6b4c2a
George Gillams <george.gillams@skyscanner.net>
2020-06-16 08:19:31 +0100

    [NO-JIRA] Add API

---
+++ server/api/api.js
@@ -0,0 +1,87 @@
+import apiStructure from './apiStructureWithActions';
+
+import { CategorisedError, InternalServerError, NotImplementedError } from 'utils/errors';
+import { mapPathToAction } from 'utils/mapPathToAction.js';
+import logger from 'utils/logger';
+
+const appFunc = (req, res) => {
+  const splitUrlPath = req.url
+    .split('?')[0]
+    .split('/')
+    .slice(1);
+
+  const pathMatches = mapPathToAction(apiStructure, splitUrlPath);
+  const pathMatchesForMethod = pathMatches.filter(c => c.apiCapability.method === req.method);
+
+  let action = null;
+  let params = [];
+
+  if (pathMatchesForMethod.length === 0 && pathMatches.length > 0) {
+    res.status(405);
+    res.json({
+      error: 'method_not_allowed',
+      errorMessage: `This API function cannot be called with HTTP method ${req.method}`,
+    });
+  }
+  if (pathMatchesForMethod.length === 1) {
+    action = pathMatchesForMethod[0].apiCapability.action;
+    params = pathMatchesForMethod[0].params;
+  }
+  if (pathMatchesForMethod.length > 1) {
+    const isError = new InternalServerError('Ambiguous request - the API has multiple functions matching the request');
+    res.status(isError.httpStatus);
+    res.json({
+      error: isError.category,
+      errorMessage: isError.message,
+    });
+  }
+
+  try {
+    if (action) {
+      console.log(`action`, action);
+      action(req, params)
+        .then(result => {
+          if (result instanceof Function) {
+            result(res);
+          } else {
+            res.json(result);
+          }
+          return true;
+        })
+        .catch(err => {
+          if (err && err.redirect) {
+            res.redirect(err.redirect);
+          } else if (err instanceof CategorisedError) {
+            res.status(err.httpStatus);
+            res.json({ error: err.category, errorMessage: err.message });
+          } else if (err instanceof Error) {
+            // An error that we haven't created. Maybe created by redis or something
+            logger.error(`Uncategorised error`, err);
+            const internalServerError = new InternalServerError(err.message);
+            res.status(internalServerError.httpStatus);
+            res.json({
+              error: internalServerError.category,
+              errorMessage: internalServerError.message,
+            });
+          } else {
+            // The error is an invalid object format, instead of being an instance of Error
+            logger.error(`LEGACY ERROR - a promise rejected an object instead of an actual Error`, err);
+            const internalServerError = new InternalServerError(err.message);
+            res.status(internalServerError.httpStatus);
+            res.json({
+              error: internalServerError.category,
+              errorMessage: internalServerError.message,
+            });
+          }
+        });
+    } else {
+      const err = new NotImplementedError('This method has not been implemented');
+      res.status(err.httpStatus);
+      res.json({ error: err.category, errorMessage: err.message });
+    }
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+export default appFunc;
--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 import { setConfig } from 'next/config';
 import nextConfig from '../next.config';
 import seo from './seo';
+import api from './api/api';
 
 const express = require('express');
 const next = require('next');
 const { Signale } = require('signale');
+import appConfig from 'helpers/appConfig';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fileupload from 'express-fileupload';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import slowDown from 'express-slow-down';
 
 setConfig(nextConfig);
 
-const dev = process.env.NODE_ENV !== 'production';
+const { NODE_ENV } = process.env;
+const dev = NODE_ENV !== 'production';
 
 const port = process.env.PORT || 3000;
 const app = next({ dev });
@@ -24,7 +33,51 @@ const signale = new Signale(options);
   await app.prepare();
   const server = express();
 
+  // Production security - helmet
+  server.use(helmet());
+
+  // Production security - cors
+  server.use(
+    cors({
+      origin: appConfig.siteUrl,
+    })
+  );
+
+  // Production security - rate limiting
+  server.use(
+    slowDown({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      delayAfter: NODE_ENV === 'production' ? 100 : 10000, // allow 50 requests per window without limiting...
+      delayMs: 500, // add 1s delay per request above 50...
+      maxDelayMs: 10000, // with a maximum delay of 10 seconds
+      // request # 1 no delay
+      // ...
+      // request # 100 no delay
+      // request # 101 is delayed by 500ms
+      // request # 102 is delayed by 1000ms
+      // request # 103 is delayed by 1500ms
+      // ...
+      // request # 120 is delayed by 10s
+      // request # 121 is delayed by 10s <-- won't exceed 10s delay
+      //
+      // The max request rate is 100 in 0s + 20 in 105s + 85 in 850s = 205 in 15 minutes = 820 in 1 hour
+      skip: req => {
+        if (req.originalUrl.includes('api')) {
+          return false;
+        }
+        return true;
+      },
+    })
+  );
+
+  // enable sending API requests with files in form-data
+  server.use(fileupload());
+
+  server.use(bodyParser.json());
+  server.use(cookieParser());
+
   server.use(seo);
+  server.use('/api', api);
   server.use('/static', express.static('public/static'));
 
   server.get('*', (req, res) => handle(req, res));
