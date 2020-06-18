c26677e8eeafd0e08571103533c445ff2dbaa35b
George Gillams <george.gillams@skyscanner.net>
2020-06-18 17:56:05 +0100

    [NO-JIRA] Separate existing containers and components

--- src/containers/Account/index.js
+++ src/containers/Account/index.js
@@ -1,32 +1,12 @@
-import React, { memo } from 'react';
-import PropTypes from 'prop-types';
+import { memo } from 'react';
 
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { useInjectReducer } from 'utils/inject-reducer';
-import { useInjectSaga } from 'utils/inject-saga';
-
-import Showcases from 'components/Showcases';
-
-import saga from './saga';
-import reducer from './reducer';
 import { getShowcases } from './actions';
 import { selectShowcases } from './selectors';
-
-export function Home({ getShowcases, showcasesData, ...rest }) {
-  useInjectSaga({ key: 'showcases', saga });
-  useInjectReducer({ key: 'showcases', reducer });
-
-  delete rest.namespacesRequired;
-
-  return (
-    <div {...rest}>
-      <Showcases onGetShowcases={getShowcases} data={showcasesData} />
-    </div>
-  );
-}
+import Home from './Container';
 
 const mapStateToProps = createStructuredSelector({
   showcasesData: selectShowcases(),
@@ -37,10 +17,5 @@ export function mapDispatchToProps(dispatch) {
 }
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-Home.propTypes = {
-  showcasesData: PropTypes.object,
-  getShowcases: PropTypes.func,
-};
 
 export default compose(withConnect, memo)(Home);
