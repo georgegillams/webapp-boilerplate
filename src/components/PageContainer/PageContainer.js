fcc293ac5ad1143090926f41efe03c81ef257da1
George Gillams <george.gillams@skyscanner.net>
2020-06-19 20:06:10 +0100

    [NO-JIRA] Migrate page container

---
+++ src/components/PageContainer/PageContainer.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { cssModules } from 'gg-components/helpers/cssModules';
+
+import STYLES from './page-container.scss';
+
+const getClassName = cssModules(STYLES);
+
+const PageContainer = props => {
+  const { className, ...rest } = props;
+  const [showDebug, setShowDebug] = useState(false);
+
+  useEffect(() => {
+    setShowDebug(window.localStorage.getItem('showPageContainerDebugColor') === 'true');
+  }, []);
+
+  const outerClassNames = [getClassName('page-container__container')];
+  if (showDebug) {
+    outerClassNames.push([getClassName('page-container__container--debug')]);
+  }
+
+  if (className) {
+    outerClassNames.push(className);
+  }
+
+  return <main className={outerClassNames.join(' ')} {...rest} />;
+};
+
+PageContainer.propTypes = {
+  className: PropTypes.string,
+};
+
+PageContainer.defaultProps = {
+  className: null,
+};
+
+export default PageContainer;
