501144b14c60fb971981dede888e15a9c83610f2
George Gillams <george.gillams@skyscanner.net>
2020-07-06 17:02:41 +0100

    [NO-JIRA] Move messageConstants

--- src/utils/messageConstants.js
+++ src/utils/messageConstants.js
@@ -1,11 +1,10 @@
-// TODO Move to client
 const GENERIC_ERROR_MESSAGE = {
   type: 'error',
   message: 'Something went wrong. Please try again later.',
 };
 const COMMUNICATION_ERROR_MESSAGE = {
   type: 'error',
-  message: 'Our servers are poorly. Please try again later.',
+  message: "The server isn't responding properly. Please try again later.",
 };
 
 export { GENERIC_ERROR_MESSAGE, COMMUNICATION_ERROR_MESSAGE };
