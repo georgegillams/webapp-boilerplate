cb81ce0a916ff4e8015bdb739ea3e51c768332c5
George Gillams <george.gillams@skyscanner.net>
2020-07-06 17:02:27 +0100

    [NO-JIRA] Remove loadable

--- src/containers/Account/Loadable.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Loadable from 'react-loadable';
-
-export default Loadable({
-  loader: () => import('./index'),
-  loading: () => <div style={{ width: '1rem', height: '1rem', background: 'red' }} />,
-});
