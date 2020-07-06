1189085295761f6db5b1df31f82b7affdcb1754d
George Gillams <george.gillams@skyscanner.net>
2020-07-06 17:03:02 +0100

    [NO-JIRA] Adopt gg-components 6.0.0 (#39)

--- src/containers/Account/Container.js
+++ src/containers/Account/Container.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageTitle from 'components/PageTitle';
-import { DebugObject, LoadingCover } from 'gg-components/Auth';
+import { LoadingCover } from 'gg-components/LoadingCover';
+import { DebugObject } from 'gg-components/DebugObject';
 import { Paragraph } from 'gg-components/Typography';
 import Button from 'components/Button';
 import { setPostLoginRedirect } from 'utils/storageHelpers';
