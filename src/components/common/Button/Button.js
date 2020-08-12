1aa42047503154ea50b718ad4afc42d478c724ce
George Gillams <george.gillams@skyscanner.net>
2020-08-12 20:20:13 +0100

    [NO-JIRA] Re-link button links

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,57 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Button as GGButton } from 'gg-components/Button';
-import Router, { useRouter } from 'next/router';
 import nextifyHref from 'utils/nextifyHref';
+import Link from 'next/link';
 
 const Button = props => {
-  const { href, hrefExternal, onClick, ...rest } = props;
-
-  const router = useRouter();
-
-  const [isServer, setIsServer] = useState(true);
+  const { href, hrefExternal, ...rest } = props;
 
   const renderNormalLink = !href || hrefExternal;
 
   const destination = nextifyHref(href);
 
-  useEffect(() => {
-    setIsServer(false);
-    if (!renderNormalLink && process.env.NODE_ENV !== 'test') {
-      Router.prefetch(destination.url, destination.as);
-    }
-  }, []);
-
-  if (isServer || renderNormalLink) {
-    return <GGButton {...props} />;
+  if (renderNormalLink) {
+    return <GGButton href={href} hrefExternal={hrefExternal} {...rest} />;
   }
 
-  const onClickFinal = async e => {
-    if (onClick) {
-      onClick(e);
-    }
-    await router.push(destination.url, destination.as, destination.options);
-    if (scroll) {
-      return window.scrollTo(0, 0);
-    }
-    return true;
-  };
-
-  return <GGButton onClick={onClickFinal} {...rest} />;
+  return (
+    <Link passHref href={destination.url} as={destination.as}>
+      <GGButton {...rest} />
+    </Link>
+  );
 };
 
 Button.propTypes = {
   href: PropTypes.string,
   hrefExternal: PropTypes.bool,
-  className: PropTypes.string,
-  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   href: null,
   hrefExternal: false,
-  className: null,
-  onClick: null,
 };
 
 export default Button;
