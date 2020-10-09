ea63c09e392adb87d2315d721d2c4386bc31fc3f
George Gillams <george.gillams@skyscanner.net>
2020-10-09 14:12:32 +0100

    [NO-JIRA] Use CSS loaders (#253)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,59 @@
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 const nextTranspileModules = require('next-transpile-modules');
 const withImages = require('next-images');
 const bundleAnalyzer = require('@next/bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const configureWebpack = (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  const mCEPLoader = {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      hmr: !!dev, // webpack 4 only
+    },
+  };
+
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        localIdentName: '[name]__[local]--[hash:base64:5]',
+      },
+    },
+  };
+
+  config.module.rules.push({
+    test: /\.css$/i,
+    use: [mCEPLoader, cssLoader],
+  });
+  config.module.rules.push({
+    test: /\.scss$/i,
+    use: [mCEPLoader, cssLoader, 'sass-loader'],
+  });
+  config.module.rules.push({
+    test: /\.(woff(2)?|ttf|eot)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          // Note these values are used to determine the URI that should be
+          // used to resolve the file, not the desired output directory.
+          // eg http://localhost:3000/static/fonts/quattrocento-sans-latin-700-normal_unused.woff2
+          name: '[name].[ext]',
+          outputPath: '/static/fonts',
+          // This loader doesn't actually include the file in the build. That is done separately
+          emitFile: false,
+        },
+      },
+    ],
+  });
+
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: dev ? 'static/[name].css' : 'static/[name].[hash].css',
+      chunkFilename: dev ? 'static/[id].css' : 'static/[id].[hash].css',
+    })
+  );
+  return config;
+};
 
 const nextConfig = {
   distDir: 'build',
@@ -17,6 +68,7 @@ const nextConfig = {
     PROJECT_UNDER_TEST: process.env.PROJECT_UNDER_TEST,
     HOST: process.env.HOST,
   },
+  webpack: configureWebpack,
 };
 
 const withTM = nextTranspileModules(['gg-components']);
@@ -29,18 +81,8 @@ module.exports = withBundleAnalyzer({});
 
 module.exports = withBundleAnalyzer(
   withImages(
-    withTM(
-      withSass(
-        withCSS({
-          cssModules: true,
-          cssLoaderOptions: {
-            importLoaders: 1,
-            url: false,
-            localIdentName: '[local]___[hash:base64:5]',
-          },
-          ...nextConfig,
-        })
-      )
-    )
+    withTM({
+      ...nextConfig,
+    })
   )
 );
