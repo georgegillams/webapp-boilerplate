8de096e47b6b863353f6b836b9dd360d94612a94
George Gillams <code@georgegillams.co.uk>
2022-05-18 20:57:37 +0100

    chore: Add JS feature detection

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import appConfig from 'helpers/appConfig';
 import { ServerStyleSheet } from 'styled-components';
+import JSFeatureDetector, { NO_JS_CLASSNAME } from '@george-gillams/components/js-feature-detector';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,7 +17,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en-GB">
+      <Html lang="en-GB" class={NO_JS_CLASSNAME}>
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/favicon-180x180.png" />
 
@@ -37,6 +38,7 @@ class MyDocument extends Document {
         </Head>
 
         <body>
+          <JSFeatureDetector />
           {/* Fixes FOUC in Firefox: https://bugzilla.mozilla.org/show_bug.cgi?id=1404468 */}
           <script>0</script>
           <Main />
