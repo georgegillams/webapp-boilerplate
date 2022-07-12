7fb513800874483e3007dc5f04f7bb1bca2ae006
George Gillams <code@georgegillams.co.uk>
2022-07-12 22:08:42 +0100

    chore: adopt ggc 28.0.0

--- src/components/common/Forms/LoginForm.js
+++ src/components/common/Forms/LoginForm.js
@@ -31,7 +31,7 @@ const LoginForm = props => {
           show: true,
           inputProps: {
             spellCheck: 'false',
-            autofill: 'email',
+            autofill: 'username',
           },
         },
       ]}
